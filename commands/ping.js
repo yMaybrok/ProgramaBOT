5f6fb69c9554f63178078e23afd3fb79b375c62f
yMaybrok <97672789+yMaybrok@users.noreply.github.com>
2022-01-13 11:32:16 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,20 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const discord = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with pong'),
-    async execute(interaction) {
-        interaction.reply({ content: 'Pong' })
-    }
+  name: "ping", //nome do comando e como ele será usado
+  category: "bot", //categoria do commando
+  description: "Comando para ver a latência do bot", //descrição do comando
+  run: async (client, message, args) => {
+    message.delete();
+
+    const m = await message.channel.send("🛠 carregando...");
+
+    m.edit(
+      "🏓|| Pong" +
+        `\nLatencia do server **${message.guild.name}**: **${m.createdTimestamp - message.createdTimestamp}ms** \n` +
+        "Latencia do Bot " +
+        `**${Math.round(client.ws.ping)}ms** velocidade deletada!`
+    );
+    //vamos testar o comando
+  }
 };
